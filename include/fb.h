437b691cf486230dca69bd014a10565c71138eb4
Jie Feng <hjf3108@gmail.com>
2024-09-18 14:39:38 +0800

    Framebuffer support

---
+++ include/fb.h
@@ -0,0 +1,75 @@
+#ifndef FB_H
+#define FB_H
+
+#include <cstdint>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+
+inline uint16_t rgb16(uint8_t r, uint8_t g, uint8_t b)
+{
+    return (r << 11) | (g << 5) | b;
+}
+
+class PAXFramebuffer
+{
+private:
+    // Framebuffer file descriptor
+    int fbfd;
+    // Framebuffer attributes
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    // Framebuffer pixels
+    uint16_t *fb;
+
+    inline uint32_t getOffset(int x, int y)
+    {
+        return (x + vinfo.xoffset) +
+               (y + vinfo.yoffset) * finfo.line_length;
+    }
+
+public:
+    PAXFramebuffer()
+    {
+        fbfd = open("/dev/fb", O_RDWR);
+        if (fbfd == -1)
+        {
+            throw "Error: cannot open framebuffer device";
+        }
+        if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1)
+        {
+            throw "Error reading fixed information";
+        }
+        if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1)
+        {
+            throw "Error reading variable information";
+        }
+        std::size_t screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+        fb = (uint16_t*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+        if (fb == (uint16_t*)-1)
+        {
+            throw "Error: failed to map framebuffer device to memory";
+        }
+    }
+
+    uint16_t& pixel(int x, int y)
+    {
+        return fb[getOffset(x, y)];
+    }
+
+    void drawPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b)
+    {
+        pixel(x, y) = rgb16(r, g, b);
+    }
+
+    ~PAXFramebuffer()
+    {
+        munmap(fb, vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8);
+        close(fbfd);
+    }
+};
+
+#endif // FB_H
